2736dc07e396b63deccafac013175dd3d253cfc3
Aulas angular <pedromogly@gmail.com>
2024-07-15 16:01:09 -0300

    0.3

--- player.gd
+++ player.gd
@@ -1,31 +1,44 @@
 extends CharacterBody3D
 
+@onready var animator = get_node("gobot/AnimationPlayer") as AnimationPlayer
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const speed = 200.0
+const JUMP_VELOCITY = 10.0
+var gravity = 0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var view:Node3D
 
- 
+var movement_eixo:Vector3
+var rotation_direction:float
+
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	handle_input(delta)
+	apply_gravity(delta)
+	jump(delta)
 	move_and_slide()
+
+func handle_input(delta):
+	var input:Vector3
+	input.x = Input.get_axis("go_esquerda","go_direita")
+	input.z = Input.get_axis("go_frente","go_atras")
+	
+	input = input.rotated(Vector3.UP, view.rotation.y).normalized()
+	 
+	velocity = input * speed * delta
+
+func apply_gravity(delta):
+	if !is_on_floor():
+		gravity += 25 * delta
+	
+	var applied_gravity:Vector3
+	applied_gravity = velocity.lerp(movement_eixo, delta * 10)
+	applied_gravity.y = -gravity
+	velocity = applied_gravity
+	
+func jump(delta):
+	if Input.is_action_just_pressed("go_pular"):
+		if is_on_floor():
+			gravity -= JUMP_VELOCITY
+	
+	if gravity > 0 and is_on_floor():
+		gravity = 0
